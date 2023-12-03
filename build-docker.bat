37a1de839250f1d5d7510bef07e89f7484350c64
William <william.harding-1@students.plymouth.ac.uk>
2023-12-03 07:00:54 +0000

    Removed storage controller from host network, passed it the DB details as environment variables instead

--- build-docker.bat
+++ build-docker.bat
@@ -6,12 +6,14 @@ docker pull mcr.microsoft.com/mssql/server:2022-latest
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=EtherealDatabaseStorage!!" -p 1433:1433 --name etherealdb --hostname etherealdb -d mcr.microsoft.com/mssql/server:2022-latest
 docker start etherealdb
 
+for /f %%i in ('docker inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} etherealdb') do set db_ip=%%i
+
 cd ./StorageController
 docker image rm storage-controller
 docker build -t storage-controller .
 docker stop storage-controller
 docker rm storage-controller
-docker run -p 8090:8090 --name storage-controller --network host --hostname storage-controller -d storage-controller
+docker run -e "DB_IP=%db_ip%" -e "DB_PASS=EtherealDatabaseStorage!!" -p 8090:8090 --name storage-controller --hostname storage-controller -d storage-controller
 
 cd %base_dir%
 
