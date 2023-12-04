0ee63d1fdab45b794cde6a07f7b106fd666c5555
William <william.harding-1@students.plymouth.ac.uk>
2023-12-04 08:25:52 +0000

    Passing the storage controller IP as an environment variable to the website container

--- build-docker.bat
+++ build-docker.bat
@@ -16,13 +16,14 @@ docker rm storage-controller
 docker run -e "DB_IP=%db_ip%" -e "DB_PASS=EtherealDatabaseStorage!!" -p 8090:8090 --name storage-controller --hostname storage-controller -d storage-controller
 
 cd %base_dir%
+for /f %%i in ('docker inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} storage-controller') do set sc_ip=%%i
 
 cd ./"Ethereal Cloud"
 docker image rm ethereal-storage
 docker build -t ethereal-storage .
 docker stop ethereal-storage
 docker rm ethereal-storage
-docker run -p 8080:8080 --name ethereal-storage --hostname ethereal-storage -d ethereal-storage
+docker run -e "SC_IP=%sc_ip%" -p 8080:8080 --name ethereal-storage --hostname ethereal-storage -d ethereal-storage
 
 cd %base_dir%
 
