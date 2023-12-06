240970bf6486853c73cdd9717e948d3aaa66d9f9
(s) William Harding 1 <wharding1@SOCNET.SOC.PLYMOUTH.AC.UK>
2023-12-06 15:08:44 +0000

    Removed in memory database usage from website

--- Ethereal Cloud/Pages/Account.cshtml.cs
+++ Ethereal Cloud/Pages/Account.cshtml.cs
@@ -8,33 +8,8 @@ namespace Ethereal_Cloud.Pages
     public class AccountModel : PageModel
     {
 
-        private readonly TestingDbContext _context;
-
-        
-        public List<Account> Accounts { get; set; } = new List<Account>();
-
-
-        [BindProperty]
-        public Account NewAccount { get; set; }
-
-        public AccountModel(TestingDbContext context)
-        {
-            _context = context;
-        }
-
-        public void OnGet()
-        {
-            Accounts = _context.Account.ToList();
-
-
-
-        }
-
         public IActionResult OnPost()
         {
-            _context.Account.Add(NewAccount);
-
-            _context.SaveChanges();
 
             return RedirectToPage();
         }
