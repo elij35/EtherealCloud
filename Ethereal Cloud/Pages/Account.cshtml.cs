da6102a41091e15aed179f7ea29e026b33dbef54
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2023-12-06 15:49:02 +0000

    Connected Website to Storage Controller

--- Ethereal Cloud/Pages/Account.cshtml.cs
+++ Ethereal Cloud/Pages/Account.cshtml.cs
@@ -8,6 +8,34 @@ namespace Ethereal_Cloud.Pages
     public class AccountModel : PageModel
     {
 
+
+        public static async Task<string> Main()
+        {
+            return await SendGetRequest();
+        }
+
+        static async Task<string> SendGetRequest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "http://" + Environment.GetEnvironmentVariable("SC_IP") + ":8090/api/auth";
+
+                HttpResponseMessage response = await client.PostAsync(url, new StringContent("email=test@test.com&password=password123"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    return content;
+                }
+                else
+                {
+                    return response.StatusCode.ToString();
+                }
+            }
+        }
+
+
         public IActionResult OnPost()
         {
 
