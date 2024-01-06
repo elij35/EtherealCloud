cc6988f5ad1188e19199dbaf1e07f17389fdb5f7
William <william.harding-1@students.plymouth.ac.uk>
2024-01-06 23:12:59 +0000

    Basic ASP.NET project for bucket

---
+++ Bucket/Program.cs
@@ -0,0 +1,34 @@
+using Bucket.Data;
+
+namespace Bucket
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run("http://[::]:8070");
+        }
+    }
+}
