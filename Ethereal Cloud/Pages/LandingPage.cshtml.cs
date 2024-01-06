d24dff3859a4da7a07f79dba1fa51ef01575035e
elij35 <elijbowen@gmail.com>
2024-01-06 21:07:54 +0000

    Created html files for each page

---
+++ Ethereal Cloud/Pages/LandingPage.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ethereal_Cloud.Pages
+{
+    public class LandlingPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
