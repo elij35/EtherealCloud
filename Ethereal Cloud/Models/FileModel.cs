4b7b1c49f223548cbe82355e352cad6ba688e4be
William <william.harding-1@students.plymouth.ac.uk>
2024-01-06 19:05:37 +0000

    FileModel for deserialization

---
+++ Ethereal Cloud/Models/FileModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ethereal_Cloud.Pages
+{
+    public class FileModel
+    {
+        public string Filename { get; set; }
+        public string Author { get; set; }
+        public string Filetype { get; set; }
+        public string Content { get; set; }
+    }
+}
