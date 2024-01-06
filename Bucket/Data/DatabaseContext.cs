5980d742ae748d67b511ee0ab80a1445d873408f
William <william.harding-1@students.plymouth.ac.uk>
2024-01-06 23:18:43 +0000

    Configuring database

---
+++ Bucket/Data/DatabaseContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bucket.Data
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public static string CONNECTION_STRING { get; private set; }
+
+        public DatabaseContext() {
+
+            CONNECTION_STRING = $"Server={Program.DB_IP};" +
+                $"User Id=SA;" +
+                $"Password={Program.DB_PASS};" +
+                $"TrustServerCertificate=true;" +
+                $"Initial Catalog=bucket_{Program.BUCK_ID};";
+
+        }
+
+        public async void EnsureTablesCreated()
+        {
+            Database.EnsureCreated();
+
+            FileData.FromSql($"SET IDENTITY_INSERT [FileData] ON;");
+            await SaveChangesAsync();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(CONNECTION_STRING);
+        }
+
+        public DbSet<FileData> FileData { get; set; }
+
+    }
+}
