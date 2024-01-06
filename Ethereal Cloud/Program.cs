cfe31371d0f994972e837ecf7ec04a64cf8b9399
elij35 <elijbowen@gmail.com>
2024-01-06 21:07:39 +0000

    General formatting

--- Ethereal Cloud/Program.cs
+++ Ethereal Cloud/Program.cs
@@ -1,14 +1,7 @@
-using Ethereal_Cloud.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
-
-
-
-
 
 var app = builder.Build();
 
