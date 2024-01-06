9c16bd3ca921a4ca6a9be3c7ace6cd9f47ebb765
William <william.harding-1@students.plymouth.ac.uk>
2024-01-06 23:15:16 +0000

    Getting environment variables for bucket

--- Bucket/Program.cs
+++ Bucket/Program.cs
@@ -5,11 +5,18 @@ namespace Bucket
     public class Program
     {
 
+        public static string DB_IP;
+        public static string DB_PASS;
+        public static string BUCK_ID;
 
         public static void Main(string[] args)
         {
 
+            DB_IP = Environment.GetEnvironmentVariable("DB_IP");
+            DB_PASS = Environment.GetEnvironmentVariable("DB_PASS");
+            BUCK_ID = Environment.GetEnvironmentVariable("BUCK_ID");
 
+            if (DB_IP == null || DB_PASS == null || BUCK_ID == null) { Environment.Exit(1); }
 
 
             var builder = WebApplication.CreateBuilder(args);
