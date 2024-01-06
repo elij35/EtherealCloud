05a905cac358d781f5df9829b7151d1bb13cf83b
William <william.harding-1@students.plymouth.ac.uk>
2024-01-06 23:22:12 +0000

    Logic for uploading a file content to the bucket

--- StorageController/BucketAPIHandler.cs
+++ StorageController/BucketAPIHandler.cs
@@ -25,6 +25,34 @@ namespace StorageController
 
         }
 
+        private struct ContentStruct
+        {
+            public string FileContent { get; set; }
+        }
+
+        public static async Task<Response<string>> SendFileContent(int fileID, string fileContent)
+        {
+
+            Response<string> responseObj;
+            using (HttpClient client = new HttpClient())
+            {
+
+                ContentStruct contentStruct = new ContentStruct()
+                {
+                    FileContent = fileContent
+                };
+
+                HttpContent content = new StringContent(JsonSerializer.Serialize(contentStruct), new MediaTypeHeaderValue("application/json"));
+                HttpResponseMessage response = await client.PostAsync($"http://{Environment.GetEnvironmentVariable("BUCK_IP")}:8070/file/{fileID}", content);
+                string responseText = await response.Content.ReadAsStringAsync();
+
+                responseObj = await Response<string>.DeserializeJSON(responseText);
+
+            }
+
+            return responseObj;
+
+        }
 
     }
 }
