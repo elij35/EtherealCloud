41c0ef0578d4bc8905e73c597c64b225e4c223fb
William <william.harding-1@students.plymouth.ac.uk>
2024-01-06 23:21:48 +0000

    Logic for getting a file content from the bucket

---
+++ StorageController/BucketAPIHandler.cs
@@ -0,0 +1,30 @@
+﻿using StorageController.Data;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace StorageController
+{
+    public class BucketAPIHandler
+    {
+
+        public static async Task<Response<string>> GetFileContent(int fileID)
+        {
+
+            Response<string> responseObj;
+            using(HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage response = await client.GetAsync($"http://{Environment.GetEnvironmentVariable("BUCK_IP")}:8070/file/{fileID}");
+                string responseText = await response.Content.ReadAsStringAsync();
+
+                responseObj = await Response<string>.DeserializeJSON(responseText);
+
+            }
+
+            return responseObj;
+
+        }
+
+
+    }
+}
