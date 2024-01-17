e5f6b4658e1d4068a80beda48988055d9d5fc389
William <william.harding-1@students.plymouth.ac.uk>
2024-01-17 01:42:20 +0000

    Passing the bucket port into the storage controller through an environment variable

--- build-docker.bat
+++ build-docker.bat
@@ -27,7 +27,7 @@ docker image rm storage-controller
 docker build -t storage-controller .
 docker stop storage-controller
 docker rm storage-controller
-docker run -e "DB_IP=%db_ip%" -e "DB_PASS=EtherealDatabaseStorage!!" -e "BUCK_IP=%buck_ip%" -p 8090:8090 --name storage-controller --hostname storage-controller -d storage-controller
+docker run -e "DB_IP=%db_ip%" -e "DB_PASS=EtherealDatabaseStorage!!" -e "BUCK_IP=%buck_ip%" -e "BUCK_PORT=8070" -p 8090:8090 --name storage-controller --hostname storage-controller -d storage-controller
 
 cd %base_dir%
 for /f %%i in ('docker inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} storage-controller') do set sc_ip=%%i
