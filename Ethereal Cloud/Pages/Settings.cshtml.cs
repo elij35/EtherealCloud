504d8accc2dff05f4913397f56482a74842b9b99
Eli Bowen <elijbowen@gmail.com>
2024-01-24 19:29:35 +0000

    Added bin and settings html & css

---
+++ Ethereal Cloud/Pages/Settings.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ethereal_Cloud.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
