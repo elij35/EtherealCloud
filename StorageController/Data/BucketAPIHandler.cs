33e70df94ceffe12066f9f7e4037f0d6c6f77f53
William <william.harding-1@students.plymouth.ac.uk>
2024-01-26 13:56:02 +0000

    Generalizing bucket API requests, using the given bucket IP and port

--- StorageController/Data/BucketAPIHandler.cs
+++ StorageController/Data/BucketAPIHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using StorageController.Data.Models;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace StorageController.Data
@@ -6,17 +7,14 @@ namespace StorageController.Data
     public class BucketAPIHandler
     {
 
-        public static async Task<Response<string>> GetFileContent(int fileID)
+        public static async Task<Response<string>> GetFileContent(int fileID, Bucket bucket)
         {
-
-            string BUCK_IP = Environment.GetEnvironmentVariable("BUCK_IP");
-            string BUCK_PORT = Environment.GetEnvironmentVariable("BUCK_PORT");
 
             Response<string> responseObj;
             using (HttpClient client = new HttpClient())
             {
 
-                HttpResponseMessage response = await client.GetAsync($"http://{BUCK_IP}:{BUCK_PORT}/file/{fileID}");
+                HttpResponseMessage response = await client.GetAsync($"http://{bucket.BucketIP}:{bucket.BucketPort}/file/{fileID}");
                 string responseText = await response.Content.ReadAsStringAsync();
 
                 responseObj = await Response<string>.DeserializeJSON(responseText);
@@ -32,7 +30,7 @@ namespace StorageController.Data
             public string FileContent { get; set; }
         }
 
-        public static async Task<Response<string>> SendFileContent(int fileID, string fileContent)
+        public static async Task<Response<string>> SendFileContent(int fileID, string fileContent, Bucket bucket)
         {
 
             Response<string> responseObj;
@@ -45,7 +43,7 @@ namespace StorageController.Data
                 };
 
                 HttpContent content = new StringContent(JsonSerializer.Serialize(contentStruct), new MediaTypeHeaderValue("application/json"));
-                HttpResponseMessage response = await client.PostAsync($"http://{Environment.GetEnvironmentVariable("BUCK_IP")}:8070/file/{fileID}", content);
+                HttpResponseMessage response = await client.PostAsync($"http://{bucket.BucketIP}:{bucket.BucketPort}/file/{fileID}", content);
                 string responseText = await response.Content.ReadAsStringAsync();
 
                 responseObj = await Response<string>.DeserializeJSON(responseText);
