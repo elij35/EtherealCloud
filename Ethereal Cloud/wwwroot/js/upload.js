ad4e30f2de5e4a6e3e642c3a55bb952036bc8c74
Eli Bowen <eli.bowen@students.plymouth.ac.uk>
2024-02-14 23:52:48 +0000

    Condensed JavaScript and removed empty lines

--- Ethereal Cloud/wwwroot/js/upload.js
+++ Ethereal Cloud/wwwroot/js/upload.js
@@ -8,3 +8,12 @@ function decodeHtml(html) {
     txt.innerHTML = html;
     return txt.value;
 }
+
+function toggleImage() {
+    var imageElement = document.getElementById('toggleImage');
+    if (imageElement.src.endsWith('sort-alpha-down.svg')) {
+        imageElement.src = 'images/tools/sort-alpha-up.svg';
+    } else {
+        imageElement.src = 'images/tools/sort-alpha-down.svg';
+    }
+}
