009a96506f8e58c07fd2527a5d460f60b27d6fa9
Eli Bowen <eli.bowen@students.plymouth.ac.uk>
2024-02-15 16:39:30 +0000

    Deleted accounts pages and upload js

--- Ethereal Cloud/wwwroot/js/upload.js
+++
@@ -1,8 +0,0 @@
-function toggleImage() {
-    var imageElement = document.getElementById('toggleImage');
-    if (imageElement.src.endsWith('sort-alpha-down.svg')) {
-        imageElement.src = 'images/tools/sort-alpha-up.svg';
-    } else {
-        imageElement.src = 'images/tools/sort-alpha-down.svg';
-    }
-}
