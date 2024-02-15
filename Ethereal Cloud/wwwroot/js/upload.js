4c5dfd03f824f3ffe6935674704fc49d3b2788a2
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-02-15 12:26:55 +0000

    Logger fix

--- Ethereal Cloud/wwwroot/js/upload.js
+++ Ethereal Cloud/wwwroot/js/upload.js
@@ -1,13 +1,4 @@
-var logger = "@ViewData["logger"]";
-if (logger !== null) {
-    console.log(decodeHtml(logger));
-}
 
-function decodeHtml(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
-}
 
 function toggleImage() {
     var imageElement = document.getElementById('toggleImage');
