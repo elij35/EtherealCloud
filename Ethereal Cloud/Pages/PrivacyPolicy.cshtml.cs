6690cf42b099fa2c62dc185ac9ce0ca7a22a92b1
Eli Bowen <eli.bowen@students.plymouth.ac.uk>
2024-02-29 19:59:35 +0000

    Created a privacy policy page

---
+++ Ethereal Cloud/Pages/PrivacyPolicy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ethereal_Cloud.Pages
+{
+    public class PrivacyPolicyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
