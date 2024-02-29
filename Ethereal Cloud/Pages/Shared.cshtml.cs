dd4825133e48d1ed28dd377a39ef9d48773b2efd
Eli Bowen <eli.bowen@students.plymouth.ac.uk>
2024-02-29 20:00:37 +0000

    Added shared page for sharing files

---
+++ Ethereal Cloud/Pages/Shared.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ethereal_Cloud.Pages
+{
+    public class SharedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
