30f1a6d727d5e8619b753a85013b92b3dfeed5c3
William <william.harding-1@students.plymouth.ac.uk>
2024-03-13 15:25:25 +0000

    Database tables for removing of folders / files

---
+++ StorageController/Data/Models/FileBin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StorageController.Data.Models
+{
+    [Table("FileBin", Schema = "ethereal")]
+    public class FileBin
+    {
+
+        [Key]
+        [Required]
+        public int FileID { get; set; }
+
+        /// <summary>
+        /// Foreign keys
+        /// </summary>
+        public FileData FileData { get; set; }
+
+    }
+}
