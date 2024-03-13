e8200473c79b76a389d7408ba90ae1381880494d
William <william.harding-1@students.plymouth.ac.uk>
2024-03-13 15:49:06 +0000

    First iterations of file move and rename controllers

---
+++ StorageController/Controllers/v2/FileRenameController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StorageController.Data;
+using StorageController.Data.Models;
+using static StorageController.Controllers.v1.FileGetController;
+
+namespace StorageController.Controllers.v2
+{
+
+    [ApiController]
+    public class FileRenameController : Controller
+    {
+
+        public struct RenameData
+        {
+            public int ObjectID { get; set; }
+            public string Name { get; set; }
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [Route("/v2/file/rename")]
+        public async Task<string> RenameFileController([FromBody] RenameData data)
+        {
+
+            string? auth = Request.Headers.Authorization.FirstOrDefault();
+
+            if (auth == null)
+                return await new Response<string>(false, "Auth Header Required.").Serialize();
+
+            Response<string> authResponse = await AuthManager.AuthorizeUser(auth);
+
+            if (!authResponse.Success)
+                return await authResponse.Serialize();
+
+            int userID = int.Parse(authResponse.Message);
+
+            DataHandler db = new();
+
+            UserFile? fileLink = db.UserFiles.Where(fileLink => fileLink.FileID == data.ObjectID && fileLink.UserID == userID).FirstOrDefault();
+
+            if (fileLink == null)
+                return await new Response<string>(false, "File link not found").Serialize();
+
+            FileData? fileData = db.Files.Where(file => file.FileID == data.ObjectID).FirstOrDefault();
+
+            if (fileData == null)
+                return await new Response<string>(false, "File not found").Serialize();
+
+            fileData.FileName = data.Name;
+            await db.SaveChangesAsync();
+
+            return await new Response<string>(true, "File name changed successfully").Serialize();
+
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [Route("/v2/folder/rename")]
+        public async Task<string> RenameFolderController([FromBody] RenameData data)
+        {
+
+            string? auth = Request.Headers.Authorization.FirstOrDefault();
+
+            if (auth == null)
+                return await new Response<string>(false, "Auth Header Required.").Serialize();
+
+            Response<string> authResponse = await AuthManager.AuthorizeUser(auth);
+
+            if (!authResponse.Success)
+                return await authResponse.Serialize();
+
+            int userID = int.Parse(authResponse.Message);
+
+            DataHandler db = new();
+
+            UserFolder? folderLink = db.UserFolders.Where(folderLink => folderLink.FolderID == data.ObjectID && folderLink.UserID == userID).FirstOrDefault();
+
+            if (folderLink == null)
+                return await new Response<string>(false, "Folder link not found").Serialize();
+
+            Folder? folderData = db.Folders.Where(folder => folder.FolderID == data.ObjectID).FirstOrDefault();
+
+            if (folderData == null)
+                return await new Response<string>(false, "Folder not found").Serialize();
+
+            folderData.FolderName = data.Name;
+            await db.SaveChangesAsync();
+
+            return await new Response<string>(true, "Folder name changed successfully").Serialize();
+
+        }
+
+    }
+}
