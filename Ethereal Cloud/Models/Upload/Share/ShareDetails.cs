5ece0ff77aad1f2f383c4fb3d75ed3e69c6cecf9
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-04-23 17:41:57 +0100

    Added share constraints

--- Ethereal Cloud/Models/Upload/Share/ShareDetails.cs
+++ Ethereal Cloud/Models/Upload/Share/ShareDetails.cs
@@ -1,9 +1,13 @@
-﻿namespace Ethereal_Cloud.Models.Upload.Share
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ethereal_Cloud.Models.Upload.Share
 {
     public class ShareDetails
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public int Id { get; set; }
     }
 }
