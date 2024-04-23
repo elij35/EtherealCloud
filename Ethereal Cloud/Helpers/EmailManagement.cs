950fc30e87b0b36b800264e80a7390bdf4094740
Riley <riley.coulstock@students.plymouth.ac.uk>
2024-04-23 20:37:56 +0100

    Added EmailManagement Tweaks

--- Ethereal Cloud/Helpers/EmailManagement.cs
+++ Ethereal Cloud/Helpers/EmailManagement.cs
@@ -15,11 +15,23 @@ namespace Ethereal_Cloud.Helpers
             client.EnableSsl = true;
 
             client.Credentials = new NetworkCredential(email, pass);
+
             Random random = new Random();
             int genCode = random.Next(100000, 1000000);
+
             MailMessage message = new MailMessage(email, userEmail);
             message.Subject = "Verification Code";
-            message.Body = $"{genCode}";
+            message.IsBodyHtml = true;
+
+            message.Body = @"<html>
+            <body>
+                <h1>Welcome to Ethereal Cloud</h1>
+                <p>If you did not create an account with us, please ignore this email</p>
+                <p>Here is your verification code: " + genCode + @"</p>
+
+            </body>
+            </html>"; 
+                
             try
             {
                 client.Send(message);
@@ -27,7 +39,7 @@ namespace Ethereal_Cloud.Helpers
             }
             catch (Exception)
             {
-                throw;
+                return null;
             }
 
             return $"{genCode}";
